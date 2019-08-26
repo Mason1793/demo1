78c5b010ca040ba863e05a771b6a7505a933cd4d
Mason_z <375359554@qq.com>
2019-08-26 21:20:15 +0800

    dd

--- src/main/java/cn/mason/demo/DemoApplication.java
+++ src/main/java/cn/mason/demo/DemoApplication.java
@@ -3,15 +3,21 @@ package cn.mason.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
 
 @SpringBootApplication
 @RestController
 public class DemoApplication {
 
-    @RequestMapping("hello")
-    public String hello(String s){
-        return "hello";
+    @RequestMapping(value = "hello",method = RequestMethod.GET)
+    public String hello(String s) throws IOException {
+        if(s.equals("0")){
+            throw new IOException("wrong");
+        }
+        return "hello:"+s;
     }
 
 
